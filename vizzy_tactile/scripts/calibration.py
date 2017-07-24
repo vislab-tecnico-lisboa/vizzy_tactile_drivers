501f3183bfcc38faa6bf99d5ad9e9f9fbdf932b5
João Avelino <joao.manuel.avelino@gmail.com>
2017-07-24 15:32:58 +0100

    Octoforce suff

--- vizzy_tactile/scripts/calibration.py
+++ vizzy_tactile/scripts/calibration.py
@@ -2,18 +2,34 @@
 # ROS Stuff
 import rospy
 import message_filters
-from vizzy_tactile.msg import TacVector, TactSensor, TactSensorArray, Tactile
+from vizzy_tactile.msg import TactSensor, TactSensorArray
 from geometry_msgs.msg import WrenchStamped
 
 # Scypy stuff
 import numpy as np
 
+global numSensors
+global calibratetList
+global state
+
 def callback(optoforce_msg, vizzy_tactsensarray_msg):
+
+    global numSensors
+
+    if numSensors < 0:
+        numSensors = len(vizzy_tactsensarray_msg)
+        print "---- Found %d sensors ----" % numSensors
+
+        for x in vizzy_tactsensarray_msg:
+            print "Found: %s" % x.frame_id
+
 
     #Very interesting stuff
     optox = optoforce_msg.wrench.force.x
     optoy = optoforce_msg.wrench.force.y
     optoz = optoforce_msg.wrench.force.z
+
+
 
 
     vizzy_x = vizzy_tactsensarray_msg[0].force.x
@@ -26,11 +42,14 @@ def callback(optoforce_msg, vizzy_tactsensarray_msg):
 
 
 rospy.init_node('tactile_calibrator')
+numSensors = -1
+state = "init"
+
+
+print "--- Welcome to the tactile sensors calibrator ---"
 
 opto_sub = message_filters.Subscriber('/optoforce_0', WrenchStamped)
-#vizzy_sub = message_filters.Subscriber('/tactileForceField', TactSensorArray)
-
-
+vizzy_sub = message_filters.Subscriber('/tactileForceField', TactSensorArray)
 
 ts = message_filters.TimeSynchronizer([opto_sub, vizzy_sub], 10)
 ts.registerCallback(callback)
